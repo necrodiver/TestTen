31d281588e1e7222a1e0b60ea85e853a5f7a1a5e
neclodiver <neclodiver@live.com>
2016-01-06 23:07:14 +0800

    添加内容

---
+++ TestTen/Common/ContractsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Common
+{
+   public class ContractsController:ApiController
+    {
+        static List<Contact> contracts;
+        static ContractsController()
+        {
+            contracts = new List<Contact>();
+        }
+    }
+}
